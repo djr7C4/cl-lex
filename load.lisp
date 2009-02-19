7617551ad3d73782f35cbfefb4d5121828911306
davidjrosenbaum2 <davidjrosenbaum2@bb95d5f2-fd40-11dd-9d1d-173cc1d06e3c>
2009-02-19 06:49:22 +0000

    first upload

---
+++ load.lisp
@@ -0,0 +1,49 @@
+;;;; Loads all files.
+;;;; Copyright (C) 2009 David J. Rosenbaum
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, under version 3 of the License
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;
+;;;; Also add information on how to contact you by electronic and paper mail.
+;;;;
+;;;; If the program does terminal interaction, make it output a short
+;;;; notice like this when it starts in an interactive mode:
+;;;;
+;;;; load.lisp Copyright (C) 2009 David J. Rosenbaum
+;;;; This program comes with ABSOLUTELY NO WARRANTY; for details see COPYING
+;;;; This is free software, and you are welcome to redistribute it
+;;;; under certain conditions; for details see COPYING
+
+(in-package :cl-user)
+
+;; This library causes a lot of warnings that don't matter.
+(declaim (sb-ext:muffle-conditions warning))
+
+(loop for file in '("packages.lisp"
+		    "specials.lisp"
+		    "util.lisp"
+		    "errors.lisp"
+		    "lexer.lisp"
+		    "parser.lisp"
+		    "regex-class.lisp"
+		    "convert.lisp"
+		    "optimize.lisp"
+		    "closures.lisp"
+		    "repetition-closures.lisp"
+		    "scanner.lisp"
+		    "api.lisp") do
+     (load (concatenate 'string "/usr/share/common-lisp/source/cl-ppcre/" file)))
+
+(dolist (file '("packages.lisp" "lex.lisp"))
+  (load (concatenate 'string "cl-lex/" file)))
+
+(declaim (sb-ext:unmuffle-conditions warning))
