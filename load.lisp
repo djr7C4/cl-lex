5f3585ca164eab2142eb9cea2d0b43e82477dde2
davidjrosenbaum2 <davidjrosenbaum2@bb95d5f2-fd40-11dd-9d1d-173cc1d06e3c>
2009-02-20 03:36:11 +0000

    changed %n to  and %0 to

--- load.lisp
+++ load.lisp
@@ -1,9 +1,9 @@
-;;;; Loads all files.
+;;;; Loads all files needed to run cl-lex code for debian systems.
 ;;;; Copyright (C) 2009 David J. Rosenbaum, email: davidjrosenbaum2@gmail.com
 ;;;;
 ;;;; This program is free software: you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation, under version 3 of the License
+;;;; the Free Software Foundation, under version 3 of the License.
 ;;;;
 ;;;; This program is distributed in the hope that it will be useful,
 ;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,10 +13,10 @@
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;
-;;;; load.lisp Copyright (C) 2009 David J. Rosenbaum
-;;;; This program comes with ABSOLUTELY NO WARRANTY; for details see COPYING
+;;;; lex.lisp Copyright (C) 2009 David J. Rosenbaum
+;;;; This program comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 ;;;; This is free software, and you are welcome to redistribute it
-;;;; under certain conditions; for details see COPYING
+;;;; under certain conditions; for details see COPYING.
 
 (in-package :cl-user)
 
